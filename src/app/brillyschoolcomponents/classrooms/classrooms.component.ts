fb8ece4496d34384843ce7f16bace95fba89677f
gmxtrr <gmxtrr@gmail.com>
2021-03-18 16:23:20 +0100

    ADDING THE IMAGE UPLOAD AND PREVIEW TO THE CLASS ROOM

--- src/app/brillyschoolcomponents/classrooms/classrooms.component.ts
+++ src/app/brillyschoolcomponents/classrooms/classrooms.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassroomsComponent implements OnInit {
 
+  //CLASS ROOM PHOTO PICKER
+  public imageSrc?: any;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    
   }
+
+  //GET THE IMAGE TO SHOW A PREVIEW FOR THE ADD NEW CLASS ROOM
+  readURL(event: any): void {
+    if (event.target?.files && event.target.files[0]) {
+        const file = event.target.files[0];
+
+        const reader = new FileReader();
+        reader.onload = e => this.imageSrc = reader.result;
+
+        reader.readAsDataURL(file);
+    }
+}
 
 }
