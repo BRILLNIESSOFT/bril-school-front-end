67ff0d0fae4d2a0a78d39eac001a57bca8ac2899
gmxtrr <gmxtrr@gmail.com>
2021-04-05 00:38:46 +0200

    Finishing the payment module using the sugested UX from @Hakim ,

--- src/app/brillyschoolcomponents/payments/master/master.component.ts
+++ src/app/brillyschoolcomponents/payments/master/master.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+//import services 
+  import { ClassService } from './../../../brillyschoolservices/classes/class.service';
+
 
 @Component({
   selector: 'app-master',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  //PAYMENT MASTER FEE
+    //GET ALL CLASSES
+    public allClasses:any[] = [];
+    //ALL SECTIONS TO THE ARRAY
+    public allSectionsToTheFee:any[] = [];
+
+
+
+
+  constructor(private FB:FormBuilder, private classesService:ClassService) { }
+
+      ngOnInit(): void {
+                          //GET ALL THE AVALIABLE CLASSES
+              this.classesService.getAllClass()
+                  .subscribe(
+                    (data:any) => this.allClasses = data.data,
+                    error => console.log("error", error)
+                    );    
+      }
+
+  //NEW MASTER FEE FORM DATA
+  paymentMasterForm = this.FB.group({
+    fee_master: this.FB.group({
+       class_section_id: [''],
+       fee_type_id: [''],
+       amount: [''],
+       due_date: [''],
+       due_date_max: [''], 
+       sections_id: []
+    })
+  });
+
+
+
+// SELECT MASTER SECTION 
+    seectMasterSection(id:any,secRef:any,classNameRef:any){
+      console.log(id + secRef + classNameRef);
+      if(this.allSectionsToTheFee.includes(id,1)){
+        this.allSectionsToTheFee.pop();
+        console.log(this.allSectionsToTheFee);
+      }else{
+        this.allSectionsToTheFee.push(id);
+        console.log(this.allSectionsToTheFee);
+      }
+
+      this.paymentMasterForm.get('fee_master.sections_id')?.setValue(this.allSectionsToTheFee);
+
+    }
+
+    
+  //ON SUBMIT NEW FEE MASTER
+  onSubmitNewpaymentMaster(){
+    console.log(this.paymentMasterForm.value);
   }
 
 }
